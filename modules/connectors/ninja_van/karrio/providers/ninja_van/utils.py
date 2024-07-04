27b5d8e4a7156f19b905127b89479e4fb42dba8b
Vatsal Chauhan <87218847+codeArtisanry@users.noreply.github.com>
2024-07-04 14:59:45 +0530

    fix token bug

--- modules/connectors/ninja_van/karrio/providers/ninja_van/utils.py
+++ modules/connectors/ninja_van/karrio/providers/ninja_van/utils.py
@@ -1,8 +1,8 @@
-
 import datetime
 import karrio.core as core
 import karrio.core.errors as errors
 import karrio.lib as lib
+import base64
 
 class Settings(core.Settings):
     """Ninja Van connection settings."""
@@ -20,13 +20,18 @@ class Settings(core.Settings):
         return (
             "https://api-sandbox.ninjavan.co"
             if self.test_mode
-            else "https://api-sandbox.ninjavan.co" #"https://api.ninjavan.co" updATE later
+            else "https://api-sandbox.ninjavan.co"  # "https://api.ninjavan.co" UPDATE later
         )
+
+    @property
+    def authorization(self):
+        pair = "%s:%s" % (self.client_id, self.client_secret)
+        return base64.b64encode(pair.encode("utf-8")).decode("ascii")
 
     @property
     def access_token(self):
         """Retrieve the access_token using the api_key|secret_key pair
-        or collect it from the cache if an unexpired access_token exist.
+        or collect it from the cache if an unexpired access_token exists.
         """
         if not all([self.client_id, self.client_secret, self.grant_type]):
             raise Exception(
@@ -34,7 +39,7 @@ class Settings(core.Settings):
             )
 
         cache_key = f"{self.carrier_name}|{self.client_id}|{self.client_secret}|{self.grant_type}"
-        now = datetime.datetime.now() + datetime.timedelta(minutes=30)
+        now = datetime.datetime.now()
 
         auth = self.connection_cache.get(cache_key) or {}
         token = auth.get("access_token")
@@ -43,36 +48,32 @@ class Settings(core.Settings):
         if token is not None and expiry is not None and expiry > now:
             return token
 
-        self.connection_cache.set(
-            cache_key,
-            lambda: login(
-                self,
-                client_id=self.client_id,
-                client_secret=self.client_secret,
-                grant_type=self.grant_type,
-            ),
+        new_auth = login(
+            self,
+            client_id=self.client_id,
+            client_secret=self.client_secret,
         )
-        new_auth = self.connection_cache.get(cache_key)
-
+        self.connection_cache.set(cache_key, new_auth)
         return new_auth["access_token"]
 
 
-def login(settings: Settings, client_id: str = None, client_secret: str = None, grant_type: str = None):
+def login(settings: Settings, client_id: str = None, client_secret: str = None):
     import karrio.providers.ninja_van.error as error
+
     result = lib.request(
         url=f"{settings.server_url}/{settings.account_country_code}/2.0/oauth/access_token",
         method="POST",
         headers={
             "content-Type": "application/json",
+            "Authorization": f"Basic {settings.authorization}",
         },
-        data=lib.to_json({"client_id": client_id, "client_secret": client_secret, "grant_type": grant_type}),
+        data=lib.to_json({"client_id": client_id, "client_secret": client_secret, "grant_type": "client_credentials"}),
     )
-    print ("====================================",client_id, "===", client_secret,settings.server_url, lib.to_dict(result) )
     response = lib.to_dict(result)
     messages = error.parse_error_response(response, settings)
     if any(messages):
         raise errors.ShippingSDKError(messages)
-    expiry = datetime.datetime.now() + datetime.timedelta(
-        seconds=float(response.get("expires_in", 0))
-    )
-    return {**response, "expiry": lib.fdatetime(expiry)}
+    expiry = datetime.datetime.now() + datetime.timedelta(seconds=float(response.get("expires_in", 0)))
+    response["expiry"] = lib.fdatetime(expiry)
+
+    return response
